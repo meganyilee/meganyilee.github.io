79dbf81d65d64d676193225a49426eba3b9f4a7e
Megan Lee <meganleek@Megans-MacBook-Pro-3.local>
2020-04-24 18:00:44 -0700

    changed font and added experience

--- js/animate.js
+++ js/animate.js
@@ -1,6 +1,20 @@
 anime({
-    targets: ".meblob",
+    targets: ".introduction .meimgs img",
     translateX: "50px",
     opacity: [0,1],
-    delay: (_, index) => 300 + index * 100,
+    delay: (_, index) => 300 + index * 200,
   });
+
+  anime({
+    targets: ".introduction .toplinks a",
+    translateX: [0, 50],
+    opacity: [0,1],
+    delay: (_, index) => 500 + index * 100,
+  });
+
+// anime({
+//   targets: ".images__container--left img",
+//   translateX: "50px",
+//   opacity: [0,1],
+//   delay: (_, index) => 300 + index * 100,
+// });
